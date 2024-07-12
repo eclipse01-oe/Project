18448515dab41a98a6269570d9ccb4c3e4ac7000
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-12 23:40:16 +0100

    ok

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let signUp= document.querryselector('#home, #sign-up')
+let signUp= document.querryselector('#home, #sign-up');
+let body= document.body;
+
+let signUpFunc= ()=> { 
+    body.style.cssText=`
+    display: relative;
+    opacity: 0.5;`;
+    signUp.style.cssText=`
+    display: absolute;
+    position: fixed;`
+}
