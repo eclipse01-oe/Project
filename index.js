1855e0bbe90b0953d19618040bb9a5664273bbe3
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-13 17:01:30 +0100

    ok

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-/*let signUp= document.querryselector('#home, #sign-up');
+let signUp= document.querryselector('#home, #sign-up');
 let body= document.body;
 
-let signUpFunc= ()=> { 
-    body.style.cssText=`
-    display: relative;
-    opacity: 0.5;`;
-    signUp.style.cssText=`
-    display: absolute;
-    position: fixed;`
-}*/
+let signUpFunc= ()=> {
+    signUp.style.cssText= `display: flex;`;
+}
+
+signUp.addEventListener("click", signUpFunc);
