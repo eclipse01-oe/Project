188a9b978889cdbaaddd90479284d37186d0784b
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-14 07:53:48 +0100

    ok

---
+++ index.js
@@ -0,0 +1,19 @@
+let signUpBtn= document.querySelector('#header > #home > #sign-up');
+let signUpPage= document.querySelector('.signUpPage')
+
+let signUpFunc= ()=> {
+    signUpPage.style.cssText= `display: flex;`;
+    }
+
+    signUpBtn.addEventListener("click", signUpFunc);
+
+    alert("welcomlet signUpBtn= document.querySelector('#header > #home > #sign-up');
+let signUpPage= document.querySelector('.signUpPage')
+
+let signUpFunc= ()=> {
+    signUpPage.style.cssText= `display: flex;`;
+}
+
+signUpBtn.addEventListener("click", signUpFunc);
+
+alert("welcome");
