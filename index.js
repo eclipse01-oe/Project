03a5f5130975c35961cfa0bff23b56ca5557ffd2
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-17 00:17:00 +0100

    ok

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let signUpBtn= document.querySelector('#Header > #Home > #sign-up');
 let signUpPage= document.getElementsByClassName('signUpPage')[0];
 const wholeBody= document.querySelectorAll('#Header, #body, #footer');
+const Body= document.querySelector('body');
 
 let signUpFunc= ()=> {
     signUpPage.style.display='flex';   
     wholeBody.forEach((element) =>{
-        element.style.cssText=
-        `filter: blur(7px);
-        overflow: hidden;`;
-    })
+        element.style.filter=' blur(7px)';
+        Body.style.overflow='hidden'; 
+    }) 
     console.log("button clicked");
     };
 
