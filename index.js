ac481a0d7bc289f91a710638178ef8b13195e006
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-17 00:29:58 +0100

    ok

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const wholeBody= document.querySelectorAll('#Header, #body, #footer');
 const Body= document.querySelector('body');
 
 let signUpFunc= ()=> {
-    signUpPage.style.display='flex';   
+    signUpPage.style.display='flex';
+    Body.style.overflow='hidden';
     wholeBody.forEach((element) =>{
         element.style.filter=' blur(7px)';
     }) 
