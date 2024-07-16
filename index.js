2edb6290086b230053f0ccae5d7753030c4c06ba
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-16 11:02:47 +0100

    ok

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let signUpBtn= document.querySelector('#Header > #Home > #sign-up');
 let signUpPage= document.getElementsByClassName('signUpPage')[0];
-let wholeBody= document.querySelectorAll('#Header, #body, #footer');
+let wholeBody= document.getElementsById('Header body footer');
 
 let signUpFunc= ()=> {
-    signUpPage.style.cssText= `display: flex;`;
+    signUpPage.style.cssText= `display: flex;`; 
     wholeBody.style.cssText= `opacity: 0.4;
     overflow-y: hidden; `;
     console.log("button clicked");
