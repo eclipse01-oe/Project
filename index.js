4182af419c9670803cbd31456714d5feeffb0777
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-21 03:03:44 +0100

    ok

--- index.js
+++ index.js
@@ -51,4 +51,6 @@ const tapScreen=  ()=> {
 
     /*cancel button to close*/
 
-    for (i=0; i<cancelBtn.length; i++){tapScreen};
+    for (i=0; i<cancelBtn.length; i++){
+        cancelBtn(i).addEventListener("click" tapScreen);
+    }; 
