4f5beda27b8a7375c2dae99146b3cb8363d65e93
eclipse01-oe <85823555+eclipse01-oe@users.noreply.github.com>
2024-07-21 02:52:07 +0100

    ok

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const signUpBtn= document.querySelector('#Header > #Home > #sign-up');
+const signUpBtn= document.querySelector('#Header > #Home > #sign-up'); 
+const loginBtn= document.querySelector('#Header > #Home > #login'); 
 const signUpPage= document.getElementsByClassName('signUpPage')[0];
 const loginPage= document.getElementsByClassName('loginPage')[0];
+const cancelBtn= document.getElementsByClassName('cancel');
 const wholeBody= document.querySelectorAll('#Header, #body, #footer');
 const Body= document.querySelector('body');
+
+/*sign up features*/
 
 const signUpFunc= ()=> {
     signUpPage.style.display='flex';
@@ -11,10 +15,26 @@ const signUpFunc= ()=> {
         element.style.cssText=`filter: blur(7px);
         overflow: hidden;`;
     }) 
-    console.log("button clicked");
+    console.log("sign up button clicked");
     };
 
     signUpBtn.addEventListener("click", signUpFunc);
+
+
+    /*login features*/
+
+    const loginFunc= ()=> {
+    loginPage.style.display='flex';
+    Body.style.overflow='hidden';
+    wholeBody.forEach((element) =>{
+        element.style.cssText=`filter: blur(7px);
+        overflow: hidden;`;
+    })
+    console.log("login button clicked");
+    };
+   
+    loginBtn.addEventListener("click", loginFunc);
+
 alert("welcome");
 
 const tapScreen=  ()=> {
